74ff47cd8affa4eedcf75d8283a3bebe8c4a9417
Pedro Bucho <pedro.bucho@gmail.com>
2016-04-26 21:26:20 +0100

    The Adeling intensifies

--- index.js
+++ index.js
@@ -81,37 +81,34 @@ controller.on('rtm_close', function (bot) {
  */
 // BEGIN EDITING HERE!
 
+var all_cases = ['direct_mention','mention','direct_message','ambient'];
+
 controller.on('bot_channel_join', function (bot, message) {
     bot.reply(message, "I'm here!")
 });
 
-controller.hears(['hello'],'ambient',function(bot,message){
-	bot.reply(message,"It's me");
-});
-
-controller.hears(['Andre','andre'],'ambient',function(bot,message){
+controller.hears(['Andre','andre'],all_cases,function(bot,message){
 	bot.reply(message,"Andre??, last time i saw him he was meeting Carlao, if you know what i mean :heart:");
 });
 
-controller.hears('rand', ['direct_mention', 'mention', 'direct_message'], function (bot, message) {
+controller.hears('rand', all_cases, function (bot, message) {
 	var frases = ["slack", "amaral", "içu"];
 	var rand = Math.floor((Math.random() * 3) + 0);
     bot.reply(message, frases[rand]);
 });
 
-controller.hears('batatas',['direct_mention', 'mention', 'direct_message'], function (bot, message) {
+controller.hears('batatas',all_cases, function (bot, message) {
 	bot.reply(message, 'fritas!');
 });
 
 controller.hears(
 	['projecto','projeto','git','commit','pull','push','branch','merge','jenkins','test','slack','fuck'],
-	'ambient',function(bot,message){
+	all_cases,function(bot,message){
 	bot.reply(message,"FLIP DA TABLE");
 	bot.reply(message,"http://www.timeanddate.com/countdown/party?iso=20160527T14&p0=133&msg=fuck+everything&ud=1&font=cursive&csz=1");
 });
 
-
-controller.hears(['pestana', 'ams'],['direct_mention', 'mention', 'direct_message'], function (bot, message) {
+controller.hears(['pestana', 'ams'],all_cases, function (bot, message) {
 	var frases = [
 		"the system must accept 5 and 10 bi.. money",
 		"you could do it, but if u are not allowded u are not allowded",
@@ -121,6 +118,67 @@ controller.hears(['pestana', 'ams'],['direct_mention', 'mention', 'direct_messag
 	];
 	var rand = Math.floor((Math.random() * 5) + 0);
     bot.reply(message, frases[rand]);
+});
+
+var adeleHears = ['hello',
+'i was wondering',
+'to go over',
+'they say the time\'s supposed to heal ya',
+'hello',
+'i\'m in california dreaming',
+'when we were younger',
+'i\'ve forgotten how it felt',
+'there\'s such a difference',
+'hello from the other side',
+'to tell you i\'m sorry',
+'but when i call you never',
+'hello from the outside',
+'to tell tou i\'m sorry',
+'but it don\'t matter it clearly'];
+
+var adeleResponds = ['it\'s me',
+'if after all these years you\'d like to meet',
+'everything',
+'but i ain\'t done much healing',
+'can you hear me?',
+'about who we used to be',
+'and free',
+'before the world fell at our feet',
+'between us, and a million miles',
+'i must have called a thousand times',
+'for everything that i\'ve done',
+'seem to be home',
+'at least i can say that i\'ve tried',
+'for breaking your heart',
+'doesn\'t tear you apart anymore'];
+
+var firstHello = true;
+var firstSorry = true;
+
+controller.hears(adeleHears,'ambient',function(bot,message){
+	var inputMessage = message.text.toLowerCase();
+	var outcomeIdx = adeleHears.indexOf(inputMessage);
+
+	if(outcomeIdx == 0){
+		if(firstHello){
+			firstHello = false;
+		}else{
+			outcomeIdx = 4;
+			firstHello = true;
+		}
+	}
+	
+	if(outcomeIdx == 10){
+		if(firstSorry){
+			firstSorry = false;
+		}else{
+			outcomeIdx = 13;
+			firstSorry = true;
+		}
+	}
+
+	var outcome = adeleResponds[outcomeIdx];
+	bot.reply(message,outcome);
 });
 
 /**
